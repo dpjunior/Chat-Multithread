a7aca3c2bb913b5504cdc262b01ba5560d9cd934
Your Name <you@example.com>
2020-03-11 15:03:53 -0300

    primeiro commit do novo chat

---
+++ src/Cliente.java
@@ -0,0 +1,117 @@
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import java.io.*;
+
+public class Cliente extends Thread{
+
+	private String ipServer;
+	private int porta;
+	
+
+	private String mensagemDigitada;
+	private Socket serverSocket;
+	private String nomeCliente;
+	private Tela tela;
+	
+	//Construtor Cliente
+	public Cliente(Tela tela){
+		this.tela = tela;
+		configurarCliente();
+	}
+	
+	//Main
+	public static void main(String[] args){
+		try{
+			Tela frame = new Tela();
+			frame.setVisible(true);
+	
+	}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+			
+}
+	//Metodo onde vai ser configurado ip, porta e caixinha de diálogo
+	public void configurarCliente(){
+		
+		JLabel ip = new JLabel("IpServer:");
+		JTextField campoIp = new JTextField("localhost");
+		JLabel porta = new JLabel("Porta:");
+		JTextField campoPorta = new JTextField("1000");
+		JLabel nome = new JLabel("Nome Cliente:");
+		JTextField campoNome = new JTextField();
+		
+		Object[] texts = {ip,campoIp, porta, campoPorta, nome, campoNome};
+		JOptionPane.showMessageDialog(null, texts);
+		
+		this.ipServer = campoIp.getText();
+		this.porta = Integer.parseInt(campoPorta.getText());
+		this.nomeCliente = campoNome.getText();
+		
+		if(this.nomeCliente.equals("") ||this.nomeCliente.equals(" ") || this.nomeCliente.equals(null)) {
+			this.nomeCliente = "NOME OCULTO";
+		}
+		this.nomeCliente = this.nomeCliente.toUpperCase();
+		  
+	}
+	
+	public void run(){
+		
+		try {
+			tela.setTitle(this.getNomeCliente());
+			
+		}catch (Exception e) {
+			this.nomeCliente = "NOME OCULTO";
+		}
+			
+			
+		try{
+			serverSocket = new Socket(ipServer, porta);
+			
+			System.out.println("Conectado a " + ipServer + ":" + porta);
+			
+			EscutadorServidor escutadorServidor = new EscutadorServidor(serverSocket.getInputStream(), this.tela);
+			escutadorServidor.start();
+			
+		} 	
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void enviarMensagem(String mensagemRecebida) {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		Date hora = Calendar.getInstance().getTime();
+		String horaFormatada = sdf.format(hora);
+		
+		try {
+			mensagemDigitada= (this.nomeCliente+": "+mensagemRecebida+" ("+horaFormatada+")");
+			if(!(mensagemDigitada.equals(""))){
+				ObjectOutputStream oo = new ObjectOutputStream(serverSocket.getOutputStream());
+				oo.writeObject(mensagemDigitada+"\n");
+				mensagemDigitada="";
+
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+		public Socket getServerSocket() {
+		return serverSocket;
+	}
+}
---
+++ src/Servidor.java
@@ -0,0 +1,62 @@
+import java.net.*;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+public class Servidor implements Runnable{
+	
+	private static Servidor servidor;
+	ServerSocket ss;
+	private Socket clienteConectado;
+	private ArrayList<Socket> clintesConectados = new ArrayList<Socket>(); 
+	
+	//Construtor servidor
+	private Servidor(int porta) throws Exception{
+		ss = new ServerSocket(porta);
+		new Thread(this).start();
+		System.out.println("Servidor ouvindo na porta:" + porta);
+	}
+	public void run(){
+		try{
+			while(true){
+				System.out.println("Aguardando cliente!!!!!!!!!!!");
+				clienteConectado = ss.accept(); 
+				// Salva no array os clientes que vao conectando  
+				clintesConectados.add(clienteConectado);
+				new TrataCliente(clienteConectado,clintesConectados).start();
+				System.out.println("Clientes conectados: "+ clintesConectados.size());
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	//Configuração do servidor
+	public static Servidor getServidor() {
+		
+		if(servidor == null) {
+			try {
+				int porta = Integer.parseInt(JOptionPane.showInputDialog("Digite a Porta:"));
+				servidor = new Servidor(porta);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		return servidor;
+	}
+	
+	//Array guarda e retorna n clientes conectados
+	public ArrayList<Socket> getClintesConectados() {
+		return clintesConectados;
+	}
+	
+	public static void main(String[] args){
+		try{
+			Servidor.getServidor();
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
